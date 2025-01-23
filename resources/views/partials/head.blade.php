4507c0712959fd1d54f071ae1dcf981dd3cfd033
danibahri <danibahri45@gmail.com>
2025-01-23 20:36:19 +0700

    make landing page

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,5 +1,6 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="icon" href="{{ asset('logo/logo_ppid.png') }}" type="image/x-icon">
 <title>@yield('title')</title>
 @vite(['resources/css/app.css', 'resources/js/app.js'])
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,92 +1,144 @@
 @extends('app')
 @section('title', 'PPID Kabupaten Sumenep')
 @section('content')
-    <div class="container">
-        <!-- Hero Section -->
-        <div class="jumbotron text-center bg-light my-4 py-5">
-            <h1 class="display-4">Selamat Datang di PPID Sumenep</h1>
-            <p class="lead">Pejabat Pengelola Informasi dan Dokumentasi Kabupaten Sumenep</p>
-            <hr class="my-4">
-            <p>Akses informasi publik dengan mudah dan transparan</p>
-            <a class="btn btn-primary btn-lg" href="#" role="button">Permohonan Informasi</a>
+    {{-- header --}}
+    <section class="bg-red-700" style="height: 75vh">
+        <div class="flex flex-col h-full w-full md:flex-row md:items-center md:justify-center">
+            <div class="mx-10 h-full flex items-center justify-center order-1 md:order-0">
+                <img src="{{ asset('logo/header2.png') }}" class="h-full md:h-screen" alt="kantor_pemda"
+                    style="filter: drop-shadow(0.5rem 0.5rem 0rem black);" />
+            </div>
+            <div class="flex flex-col items-center justify-center h-full order-0 md:w-1/2 md:order-1">
+                <h1 class="text-white font-bold text-3xl p-5 my-5 text-center md:text-left md:text-6xl">Selamat Datang di
+                    Portal PPID
+                    Kabupaten Sumenep
+                </h1>
+            </div>
         </div>
+    </section>
 
-        <div class="row mb-4">
-            <div class="col-12">
-                <h2 class="text-center mb-4">Layanan Kami</h2>
-            </div>
-            <div class="col-md-4">
-                <div class="card">
-                    <div class="card-body text-center">
-                        <i class="fas fa-file-alt fa-3x mb-3 text-primary"></i>
-                        <h5 class="card-title">Permohonan Informasi</h5>
-                        <p class="card-text">Ajukan permohonan informasi publik secara online</p>
-                        <a href="#" class="btn btn-outline-primary">Selengkapnya</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card">
-                    <div class="card-body text-center">
-                        <i class="fas fa-chart-bar fa-3x mb-3 text-primary"></i>
-                        <h5 class="card-title">Data & Statistik</h5>
-                        <p class="card-text">Lihat data dan statistik informasi publik</p>
-                        <a href="#" class="btn btn-outline-primary">Selengkapnya</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card">
-                    <div class="card-body text-center">
-                        <i class="fas fa-newspaper fa-3x mb-3 text-primary"></i>
-                        <h5 class="card-title">Berita Terkini</h5>
-                        <p class="card-text">Update informasi dan berita terbaru</p>
-                        <a href="#" class="btn btn-outline-primary">Selengkapnya</a>
-                    </div>
+    <section class="max-w-screen-xl mx-auto my-40 p-10 md:p-0">
+        <div class="mx-auto">
+            <div class="p-4">
+                <h1 class="font-bold text-2xl md:text-4xl my-10">Layanan Publik</h1>
+                <div class="grid grid-cols-2 gap-2 md:grid-cols-4 md:gap-8">
+                    <a href="#"
+                        class="flex flex-col justify-between items-center w-full bg-red-700 text-white p-4 hover:scale-105 transition-transform duration-300">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="white" class="bi bi-chat-right-text w-24 h-24 mb-2"
+                            viewBox="0 0 16 16">
+                            <path
+                                d="M2 1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h9.586a2 2 0 0 1 1.414.586l2 2V2a1 1 0 0 0-1-1zm12-1a2 2 0 0 1 2 2v12.793a.5.5 0 0 1-.854.353l-2.853-2.853a1 1 0 0 0-.707-.293H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z" />
+                            <path
+                                d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5M3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6m0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5" />
+                        </svg>
+                        <span class="text-center font-bold text-sm md:text-xl truncate">Lapor</span>
+                    </a>
+                    <a href="#"
+                        class="flex flex-col justify-between items-center w-full bg-red-700 text-white p-4 hover:scale-105 transition-transform duration-300">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="white" class="bi bi-hospital w-24 h-24 mb-2"
+                            viewBox="0 0 16 16">
+                            <path
+                                d="M8.5 5.034v1.1l.953-.55.5.867L9 7l.953.55-.5.866-.953-.55v1.1h-1v-1.1l-.953.55-.5-.866L7 7l-.953-.55.5-.866.953.55v-1.1zM13.25 9a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25zM13 11.25a.25.25 0 0 1 .25-.25h.5a.25.25 0 0 1 .25.25v.5a.25.25 0 0 1-.25.25h-.5a.25.25 0 0 1-.25-.25zm.25 1.75a.25.25 0 0 0-.25.25v.5c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25v-.5a.25.25 0 0 0-.25-.25z" />
+                            <path
+                                d="M5 1a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1a1 1 0 0 1 1 1v4h3a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V8a1 1 0 0 1 1-1h3V3a1 1 0 0 1 1-1zm2 14h2v-3H7zm3 0h1V3H5v12h1v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1zm0-14H6v1h4zm2 7v7h3V8zm-8 7V8H1v7z" />
+                        </svg>
+                        <span class="text-center font-bold text-sm md:text-xl truncate">Klinik Hoax</span>
+                    </a>
+                    <a href="#"
+                        class="flex flex-col justify-between items-center w-full bg-red-700 text-white p-4 hover:scale-105 transition-transform duration-300">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="white" class="bi bi-people w-24 h-24 mb-2"
+                            viewBox="0 0 16 16">
+                            <path
+                                d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4" />
+                        </svg>
+                        <span class="text-center font-bold text-sm md:text-xl truncate">Survey Kepuasan</span>
+                    </a>
+                    <a href="#"
+                        class="flex flex-col justify-center items-center w-full bg-red-700 text-white p-4 hover:scale-105 transition-transform duration-300">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-lightning w-24 h-24"
+                            viewBox="0 0 16 16">
+                            <path
+                                d="M5.52.359A.5.5 0 0 1 6 0h4a.5.5 0 0 1 .474.658L8.694 6H12.5a.5.5 0 0 1 .395.807l-7 9a.5.5 0 0 1-.873-.454L6.823 9.5H3.5a.5.5 0 0 1-.48-.641zM6.374 1 4.168 8.5H7.5a.5.5 0 0 1 .478.647L6.78 13.04 11.478 7H8a.5.5 0 0 1-.474-.658L9.306 1z" />
+                        </svg>
+                    </a>
                 </div>
             </div>
         </div>
+    </section>
 
-        <div class="row mb-4">
-            <div class="col-12">
-                <h2 class="text-center mb-4">Informasi Terbaru</h2>
-            </div>
-            <div class="col-md-6">
-                <div class="card mb-3">
-                    <div class="card-body">
-                        <h5 class="card-title">Pengumuman</h5>
-                        <ul class="list-unstyled">
-                            <li class="mb-2">
-                                <a href="#">Pengumuman Layanan PPID Tahun 2023</a>
-                            </li>
-                            <li class="mb-2">
-                                <a href="#">Jadwal Pelayanan PPID</a>
-                            </li>
-                            <li class="mb-2">
-                                <a href="#">Prosedur Permohonan Informasi</a>
-                            </li>
-                        </ul>
-                    </div>
+    <section class="w-full h-64 bg-red-700">
+        <div class="max-w-screen-xl mx-auto p-10 md:px-0 md:py-5">
+            <h1 class="text-white font-bold text-2xl mb-3">Permohonan Informasi</h1>
+            <div class="grid grid-cols-2 md:grid-cols-4 gap-10">
+                <div class="w-full p-6 bg-white border border-gray-200 rounded-sm shadow">
+                    <a href="javascript:void(0)">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Noteworthy technology
+                            acquisitions 2021</h5>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-700">Here are the biggest enterprise technology
+                        acquisitions of 2021 so far, in reverse chronological order.</p>
+                    <a href="javascript:void(0)"
+                        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-sm hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                        Read more
+                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M1 5h12m0 0L9 1m4 4L9 9" />
+                        </svg>
+                    </a>
                 </div>
-            </div>
-            <div class="col-md-6">
-                <div class="card mb-3">
-                    <div class="card-body">
-                        <h5 class="card-title">Berita Terkini</h5>
-                        <ul class="list-unstyled">
-                            <li class="mb-2">
-                                <a href="#">Launching Website PPID Sumenep</a>
-                            </li>
-                            <li class="mb-2">
-                                <a href="#">Sosialisasi Keterbukaan Informasi Publik</a>
-                            </li>
-                            <li class="mb-2">
-                                <a href="#">Pelatihan Admin PPID</a>
-                            </li>
-                        </ul>
-                    </div>
+                <div class="w-full p-6 bg-white border border-gray-200 rounded-sm shadow">
+                    <a href="javascript:void(0)">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Noteworthy technology
+                            acquisitions 2021</h5>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-700">Here are the biggest enterprise technology
+                        acquisitions of 2021 so far, in reverse chronological order.</p>
+                    <a href="javascript:void(0)"
+                        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-sm hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                        Read more
+                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M1 5h12m0 0L9 1m4 4L9 9" />
+                        </svg>
+                    </a>
+                </div>
+                <div class="w-full p-6 bg-white border border-gray-200 rounded-sm shadow">
+                    <a href="javascript:void(0)">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Noteworthy technology
+                            acquisitions 2021</h5>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-700">Here are the biggest enterprise technology
+                        acquisitions of 2021 so far, in reverse chronological order.</p>
+                    <a href="javascript:void(0)"
+                        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-sm hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                        Read more
+                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M1 5h12m0 0L9 1m4 4L9 9" />
+                        </svg>
+                    </a>
+                </div>
+                <div class="w-full p-6 bg-white border border-gray-200 rounded-sm shadow">
+                    <a href="javascript:void(0)">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Noteworthy technology
+                            acquisitions 2021</h5>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-700">Here are the biggest enterprise technology
+                        acquisitions of 2021 so far, in reverse chronological order.</p>
+                    <a href="javascript:void(0)"
+                        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-sm hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                        Read more
+                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M1 5h12m0 0L9 1m4 4L9 9" />
+                        </svg>
+                    </a>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 @endsection
