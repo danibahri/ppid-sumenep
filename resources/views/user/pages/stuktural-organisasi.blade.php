88cda54bd85e57dafe56cde09dfb4b2da7ef01cd
AdiRahman2003 <adirahman.eu123@gmail.com>
2025-01-30 11:52:34 +0700

    informasi

--- resources/views/user/pages/stuktural-organisasi.blade.php
+++ resources/views/user/pages/stuktural-organisasi.blade.php
@@ -1,19 +1,19 @@
 @extends('app')
 @section('title', 'PPID Kabupaten Sumenep')
 @section('content')
-    <div class=" flex justify-center items-center w-screen h-screen p-5 my-40">
-        <div class="border bg-white shadow-teal-300 shadow-md max-w-2xl p-6 rounded-lg hover:border-3">
-            <h2 class="text-4xl font-mono font-extrabold py-3 text-center text-red-500">STUKTURAL PPID SUMENEP</h2>
-            <div class="flex justify-center mt-1">
-                <img src="{{ asset('logo/stukturalppid.jpg') }}" alt="dummyimage" class="my-2" />
-            </div>
-            <p
-                class="border shadow-teal-300 shadow-md max-w-2xl p-6 rounded-lg hover:border-3 hover:text-white hover:bg-red-600">
-                Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for
-                previewing layouts and visual mockups....
-
-            </p>
-
+<div class=" flex justify-center items-center w-screen h-screen p-5 my-40">
+    <div class="border bg-white shadow-teal-300 shadow-md max-w-2xl p-6 rounded-lg hover:border-3">
+        <h2 class="text-4xl font-mono font-extrabold py-3 text-center text-red-500">STUKTURAL PPID SUMENEP</h2>
+        <div class="flex justify-center mt-1">
+            <img src="{{ asset('logo/stukturalppid.jpg') }}" alt="dummyimage" class="my-2" />
         </div>
+        <p
+            class="border shadow-teal-300 shadow-md max-w-2xl p-6 rounded-lg hover:border-3 hover:text-white hover:bg-red-600">
+            Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for
+            previewing layouts and visual mockups....
+
+        </p>
+
     </div>
+</div>
 @endsection
